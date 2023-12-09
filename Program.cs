4c8b74facaf51b0477338d3030c9e2c36cb22769
Marta <martra04201998@icloud/com>
2023-12-09 14:47:53 +0300

    задач3 готова

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ namespace dz1{
             Console.WriteLine(sum); */
 
             //задание 2
-          int sum = 0;
+          /*int sum = 0;
             
             int [] arr = new int [10] ;
             for (int i = 0 ; i< arr.Length; i++)
@@ -35,7 +35,43 @@ namespace dz1{
                 
             }    
             Console.WriteLine();
-           Console.WriteLine("Количество четных чисел в массиве:"+sum); 
+           Console.WriteLine("Количество четных чисел в массиве:"+sum); */
+
+
+           //ЗАДАЧА 3
+
+           Console.Write("Введите количество элементов массива: ");
+        int n = int.Parse(Console.ReadLine());
+        double[] array = new double[n];
+        Random random = new Random();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(100) + random.NextDouble(); // Генерация числа до 100 с ненулевой дробной частью
+        }
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Элемент {i + 1}: {array[i]}");
+        }
+         double max = array[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
+        }
+         double min = array[0];
+        for (int i = 1; i > n; i++)
+        {
+            if (array[i] < min)
+            {
+                min = array[i];
+            }
+        }
+        Console.WriteLine("Максимальное значение:"+ max);
+         Console.WriteLine("Минимальное значение:"+ min);
+
         }
     }
 }
